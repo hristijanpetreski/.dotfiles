6cde3505e2500dee679314c8336c8bc21fcbb634
Hristijan <72232193+hristijanpetreski@users.noreply.github.com>
2022-11-28 15:33:43 +0100

    added scrolloff and signcolumn in vim config

--- .config/nvim/lua/hriphaestus/set.lua
+++ .config/nvim/lua/hriphaestus/set.lua
@@ -4,18 +4,17 @@ vim.opt.fileencoding = "utf-8"
 
 vim.opt.guicursor = ""
 
-vim.opt.nu = true
-vim.opt.relativenumber = true
-
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
-
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 8
+vim.opt.shiftwidth = 4
 vim.opt.smartindent = true
-
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
 vim.opt.wrap = false
+vim.signcolumn = 'yes'
 
 vim.g.mapleader = " "
