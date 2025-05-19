dd7e101c03049c415e7cb17484997e6b884f6275
Hristijan Petreski <hristijanpetreski@outlook.com>
2025-05-20 01:26:26 +0200

    Update packages and lsp config

--- nvim/.config/nvim/lua/hriphaestus/plugins/lsp.lua
+++ nvim/.config/nvim/lua/hriphaestus/plugins/lsp.lua
@@ -15,6 +15,7 @@ return {
 			{ "williamboman/mason.nvim", config = true },
 			"williamboman/mason-lspconfig.nvim",
 			"hrsh7th/cmp-nvim-lsp",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
 		},
 		config = function()
 			vim.api.nvim_create_autocmd("LspAttach", {
@@ -56,8 +57,27 @@ return {
 
 					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
+					---@param client vim.lsp.Client
+					---@param method vim.lsp.protocol.Method
+					---@param bufnr? integer some lsp support methods only specific files
+					---@return boolean
+					local function client_supports_method(client, method, bufnr)
+						if vim.fn.has("nvim-0.11") == 1 then
+							return client:supports_method(method, bufnr)
+						else
+							return client.supports_method(method, { bufnr = bufnr })
+						end
+					end
+
 					local client = vim.lsp.get_client_by_id(event.data.client_id)
-					if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+					if
+						client
+						and client_supports_method(
+							client,
+							vim.lsp.protocol.Methods.textDocument_documentHighlight,
+							event.buf
+						)
+					then
 						local highlight_augroup =
 							vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
 						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
@@ -81,7 +101,7 @@ return {
 						})
 					end
 
-					if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+					if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint) then
 						map("<leader>th", function()
 							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
 						end, "[T]oggle Inlay [H]ints")
@@ -116,13 +136,16 @@ return {
 			require("mason").setup()
 
 			local ensure_installed = vim.tbl_keys(servers or {})
-			vim.list_extend(ensure_installed, {
+			local tools_and_formatters = {
 				"stylua",
-				"prettier",
 				"rubocop",
-			})
+				"prettier",
+			}
+
+			require("mason-tool-installer").setup({ ensure_installed = tools_and_formatters })
 
 			require("mason-lspconfig").setup({
+				automatic_enable = true,
 				automatic_installation = true,
 				ensure_installed = ensure_installed,
 				handlers = {
